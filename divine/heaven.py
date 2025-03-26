b3bf45dc1db7626a320c455906820e68cd53971e
biscuit <nayyaungoo@hotmail.com>
2025-03-26 23:30:59 +0630

    Add styles: border

--- divine/heaven.py
+++ divine/heaven.py
@@ -1,11 +1,17 @@
 import curses
+from box import Box
 
 
 class Heaven(object):
 
     def _Layout(self):
         """ Custom layout configurations can be written here. If
-            not specified, the default layouts will take over
+            not specified, the default configurations will take over
+        """
+
+    def _Border(self):
+        """ Custom border configurations can be written here. If
+            not specified, the default configurations will take over
         """
 
     def main(self):
@@ -20,14 +26,21 @@ class Heaven(object):
         # Initialize the screen
         self.screen = curses.initscr()
 
-        # Apply Default Configurations
+        # Define Default Configurations
         self.__Default_Configurations()
 
         # Overwrite the Default Configurations if specified
         self._Layout()
+        self._Border()
 
         # Recreate the screen, ready to utilize
         self.screen = curses.newwin(self.maxy, self.maxx, self.begy, self.begx)
+
+        # Apply Configurations
+        if self.border.all != None:
+            for border_ch in self.border:
+                self.border[border_ch] = self.border.all
+        self.__draw_border() if self.has_border else self.__erase_border()
 
     def stop(self):
         """ Deconstruct the screen, restoring the default Terminal
@@ -42,5 +55,35 @@ class Heaven(object):
         self.stop()
 
     def __Default_Configurations(self):
-        self.maxy, self.maxx = self.screen.getmaxyx()
-        self.begy, self.begx = self.screen.getbegyx()
+      # def _Layouts(self):
+            self.maxy, self.maxx = self.screen.getmaxyx()
+            self.begy, self.begx = self.screen.getbegyx()
+
+      # def _Border(self):
+            self.has_border = False
+            self.border = Box()
+            self.border.all          = None
+            self.border.left         = curses.ACS_VLINE
+            self.border.right        = curses.ACS_VLINE
+            self.border.top          = curses.ACS_HLINE
+            self.border.bottom       = curses.ACS_HLINE
+            self.border.top_left     = curses.ACS_ULCORNER
+            self.border.top_right    = curses.ACS_URCORNER
+            self.border.bottom_left  = curses.ACS_LLCORNER
+            self.border.bottom_right = curses.ACS_LRCORNER
+
+    def __draw_border(self):
+        # TODO: Make a own border function, the Curses one does not support all characters. (Raises OverflowError)
+        self.screen.border(
+            self.border.left,
+            self.border.right,
+            self.border.top,
+            self.border.bottom,
+            self.border.top_left,
+            self.border.top_right,
+            self.border.bottom_left,
+            self.border.bottom_right,
+        )
+
+    def __erase_border(self):
+        self.screen.border(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ')
