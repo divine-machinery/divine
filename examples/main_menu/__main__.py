a9d55baab8ff16b89e2a22645e14d67eb8c894b0
biscuit <nayyaungoo@hotmail.com>
2025-03-29 19:43:22 +0630

    Refactor examples/main_menu

--- examples/main_menu/__main__.py
+++ examples/main_menu/__main__.py
@@ -1,8 +1,3 @@
-# NOTE <------------------------------------------------------------------- !!!
-# This is still in experiment stage, preferably a prototype, even though this 
-# was pushed into main brach due to my lack of experiences with git, this needs
-# a lot of refinements.
-
 import time
 from divine import Heaven
 
@@ -25,45 +20,28 @@ class _MainMenu(Heaven):
     def main(self):
         
         def draw_menu(menu):
-            self.write()
             for index, item in enumerate(menu):
-                self.write(f"{index}.{item}", tag='menu_item')
+                self.write(f"{index}.{item}")
 
         while True:
-            self.write("> examples/main_menu.py", tag='filepath norm_text')
-            self.write("Hello, World! Welcome Human!", tag='welcoming_message')
+            self.write("> examples/main_menu.py", tag='awesome', top=2)
+            self.write("Hello, World! Welcome Human!", bottom=1)
             draw_menu(self.menu)
 
-            guess = self.ask("> Enter your guess(0/1/2/3): ", desired=int, informative=True, tag='question', pullx=False)
+            guess = self.ask("> Enter your guess(0/1/2/3): ", desired=int, informative=True, top=1)
 
             if guess.fullfilled and guess.answer in (0, 1, 2, 3):
-                self.write("│ Oh okay...", tag='farewell norm_text')
+                self.write("│ Oh okay...", top=1)
                 self.write("│ Just remember. I always loved you<3")
-                self.write("│ Exiting..", tag='log')
+                self.write("│ Exiting..")
                 time.sleep(3)
                 break
         
             self.reset()
 
     def _styles(self):
-        self.tag.norm_text = {
+        self.tag.awesome = {
             'left': 2,
-        }
-
-        self.tag.filepath = {
-            'top': 1,
-        }
-
-        self.tag.question = {
-            'top': 2,
-        }
-
-        self.tag.farewell = {
-            'top': 1,
-        }
-
-        self.tag.log = {
-            'top': 1,
         }
 
 MainMenu = _MainMenu()
