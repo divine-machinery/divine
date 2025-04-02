40d9bfbd01e374bb4ab4132bfe798e35aaf5bedb
biscuit <nayyaungoo@hotmail.com>
2025-04-02 18:35:36 +0630

    Add a function that converts RGB channel to Curses RGB channel

--- divine/colors/__init__.py
+++ divine/colors/__init__.py
@@ -1,2 +1,2 @@
-from .rgb import rgb, rgb_to_hexatone
+from .rgb import rgb, rgb_to_hexatone, rgb_to_termcolor
 from .hexatone import hexatone, hexatone_to_rgb
--- divine/colors/rgb.py
+++ divine/colors/rgb.py
@@ -1,3 +1,4 @@
+from math import ceil
 from .base import ColorModel, ChannelError
 
 class rgb(ColorModel):
@@ -24,3 +25,11 @@ def rgb_to_hexatone(rgb_channel):
     elif (102 < rgb_channel <= 153): return 3
     elif (153 < rgb_channel <= 204): return 4
     elif (204 < rgb_channel <= 255): return 5
+
+def rgb_to_termcolor(rgb_channel):
+    if not isinstance(rgb_channel, int) or not (0 <= rgb_channel <= 255): 
+        raise ChannelError(
+            f"Invalid RGB Channel. Expected 'int' type between 0 and 255, got {rgb_channel} as '{type(rgb_channel).__name__}' type."
+        )
+
+    return ceil(rgb_channel/255 * 1000)
