a07feedbdede677392035f34f3330c1989510a30
biscuit <nayyaungoo@hotmail.com>
2025-04-06 01:05:21 +0630

    Update examples/apps/main_menu/__main__.py.
    [~] Update examples/apps/main_menu/__main__.py:
    	Synchronize the changes

--- examples/apps/main_menu/__main__.py
+++ examples/apps/main_menu/__main__.py
@@ -14,20 +14,20 @@ class _MainMenu(Heaven):
         
         def draw_menu(menu):
             for index, item in enumerate(menu):
-                self.write(f"{index}.{item}")
+                self.write(f"{index}.{item}", tag='awesome')
 
         while True:
             self.barrier(activate=True)
-            self.write("> examples/main_menu.py", tag='awesome', top=2)
-            self.write("Hello, World! Welcome Human!", bottom=2)
+            self.write("> examples/main_menu.py", tag='awesome', top=2, bottom=2, pullyx=True, right=2)
+            self.write("Hello, World! Welcome Human!", bottom=1)
             draw_menu(self.menu)
 
-            guess = self.ask("> Enter your guess(0/1/2/3): ", desired=int, informative=True, top=1)
+            guess = self.ask("> Enter your guess(0/1/2/3): ", desired=int, informative=True, top=1, tag='awesome')
 
             if guess.fullfilled and guess.answer in (0, 1, 2, 3):
-                self.write("│ Oh okay...", top=1)
-                self.write("│ Just remember. I always loved you<3")
-                self.write("│ Exiting..")
+                self.write("│ Oh okay...", tag='awesome nice')
+                self.write("│ Just remember. I always loved you<3", tag='awesome')
+                self.write("│ Exiting..", tag='awesome')
                 time.sleep(3)
                 break
         
@@ -36,6 +36,9 @@ class _MainMenu(Heaven):
     def styles(self):
         self.tag.awesome = {
             'left': 4,
+        }
+        self.tag.nice = {
+            'top': 1
         }
         self.border.all = 'X'
 
