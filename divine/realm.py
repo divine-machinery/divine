a902fa65d810d99f1f3bbc4909c8a9f1215eb3b9
biscuit <nayyaungoo@hotmail.com>
2025-04-05 18:43:32 +0630

    Reformat comments. Comment out the purposeless code. Correct Typo.
    [~] Reformat comments:
    	Tried the extension 'Better Comments', never again :>
    
    [-] Comment out update_cursor('x'), on line 217 of divine/realm.py:
    	Seem to be very purposeless. Will be commented out at least until something goes wrong, otherwise, will be removed.
    
    [~] Fix Typo, on line 222 of divine/realm.py:
    	'trightop' should be 'right'.

--- divine/realm.py
+++ divine/realm.py
@@ -26,20 +26,20 @@ class Realm(object):
             no longer accessible until it is deconstructed.
         """
 
-        #! Apply Default Configurations
+        # Apply Default Configurations
         self.__Default_Configurations()
 
-        #! Apply Internal Stylings, overide the Default Configurations
+        # Apply Internal Stylings, overide the Default Configurations
         self.styles()
 
-        #! Validate the layouts. Raise InvalidLayout 
-        #! if custom layouts are below zero 
+        # Validate the layouts. Raise InvalidLayout 
+        # if custom layouts are below zero 
         self.__validate_Layout()
 
-        #! Spawn a realm to display output
+        # Spawn a realm to display output
         self.summon()
 
-        #! Enable Echo mode, specifically for ask()
+        # Enable Echo mode, specifically for ask()
         curses.echo()
 
     def stop(self):
@@ -72,7 +72,7 @@ class Realm(object):
         self.refresh()
 
 
-#$ -----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 
     def write(
@@ -93,49 +93,49 @@ class Realm(object):
 
     ) -> None:
 
-        #! Validate the coordinates
+        # Validate the coordinates
         if len(coordinates) not in (0, 2):
             raise InvalidCoordinate(
                 f"Expected zero to two coordinates. Got {coordinates}"
             )
 
-        #! If received no coordinates, automatically 
-        #! assign coordinates by tracking the cursors
+        # If received no coordinates, automatically 
+        # assign coordinates by tracking the cursors
         elif len(coordinates) == 0:
 
-            #! Assign the coordinates to write on
+            # Assign the coordinates to write on
             y = self.cursor.y + self.has_border
             x = self.cursor.x + self.has_border
 
 
-        #! If received two coordinates, simply assign those
-        #! coordinates.(What else you expect anyway.. XD)
+        # If received two coordinates, simply assign those
+        # coordinates.(What else you expect anyway.. XD)
         elif len(coordinates) == 2:
 
-            #| But wait, validate the type of received coordinates first.
-            #| Gotta raise TypeError if they aren't 'int' types :< 
+            # But wait, validate the type of received coordinates first.
+            # Gotta raise TypeError if they aren't 'int' types :< 
             if not isinstance(coordinates[0], int) or not isinstance(coordinates[1], int):
                 raise TypeError(
                     f"Expected coordinates as intgers. Got {type(coordinates[0]).__name__}, {type(coordinates[1]).__name__}."
                 )
 
-            #! Assign the coordinates to write on
+            # Assign the coordinates to write on
             y = coordinates[0] + self.has_border
             x = coordinates[1] + self.has_border
 
 
-        #$ -----------------------------------------------------------------
+        # -----------------------------------------------------------------
 
-        ### INTERNAL FUNCTIONS <--------------------------------------- > w <
+        # INTERNAL FUNCTIONS <--------------------------------------- > w <
 
-        #! Can safely create internal functions, assuming
-        #! no Error can be produced at this point :>
+        # Can safely create internal functions, assuming
+        # no Error can be produced at this point :>
 
         def inline_styled(property):
-            #! Checked if a property has inline-styled; Determine
-            #! by if the property has passed in write()'s arguments 
+            # Checked if a property has inline-styled; Determine
+            # by if the property has passed in write()'s arguments 
 
-            #| I just love switch cases sooo MUCH :3
+            # I just love switch cases sooo MUCH :3
             match property:
                 case 'top': return top != 0
                 case 'bottom': return bottom != 0
@@ -143,7 +143,7 @@ class Realm(object):
                 case 'right': return right != 0
 
         def update_cursor(axis):
-            #| Self explanatory, innit? XD
+            # Self explanatory, innit? XD
 
             match axis:
                 case 'y': self.cursor.y = y - self.has_border + leading
@@ -153,54 +153,54 @@ class Realm(object):
                     f"Expected 'x' or 'y'. Got {axis}." 
                 )
 
-        #$ -----------------------------------------------------------------
+        # -----------------------------------------------------------------
 
-        #! Update all the cursors in different cases
+        # Update all the cursors in different cases
         if pully: update_cursor('y')
         if pullx: update_cursor('x')
         if not pullx: self.cursor.reset('x') #! Reset to 0
 
-        #$ -----------------------------------------------------------------
+        # -----------------------------------------------------------------
 
-        ### INTERNAL STYLINGS <--------------------------------------- > w <
+        # INTERNAL STYLINGS <--------------------------------------- > w <
 
-        #! Separate the tags
+        # Separate the tags
         tags = tag.split()
 
-        #! Apply internal-stylings for each tags
+        # Apply internal-stylings for each tags
         for tag in tags:
 
-            #! Check if received tag is styled in some way
+            # Check if received tag is styled in some way
             if tag in self.tag.keys():
 
                 # NOTE <----------------------------------------------------- !!!
-                #  * The cursor is specifically need to update, at any cases
-                #  * Inline styling should and will overide the internal-stylings
+                # The cursor is specifically need to update, at any cases
+                # Inline styling should and will overide the internal-stylings
 
-                ### STYLE: top
+                # STYLE: top
                 if self.__tag_has_property(tag, has='top') and not inline_styled('top'): 
                     y += self.tag[tag]['top']
                     update_cursor('y')
 
-                ### STYLE: bottom
+                # STYLE: bottom
                 if self.__tag_has_property(tag, has='bottom') and not inline_styled('bottom'): 
                     self.cursor.y += self.tag[tag]['bottom']
 
-                ### STYLE: left
+                # STYLE: left
                 if self.__tag_has_property(tag, has='left') and not inline_styled('left'): 
                     x += self.tag[tag]['left']
                     update_cursor('x')
 
-                ### STYLE: right
+                # STYLE: right
                 if self.__tag_has_property(tag, has='right') and not inline_styled('right'): 
                     text = text + " " * self.tag[tag]['right']
 
 
-        #$ -----------------------------------------------------------------
+        # -----------------------------------------------------------------
 
-        ### INLINE STYLINGS <--------------------------------------- > w <
+        # INLINE STYLINGS <--------------------------------------- > w <
 
-        #! Apply inline-stylings
+        # Apply inline-stylings
 
         if pullyx:
             self.cursor.y = y - self.has_border - 1
@@ -214,15 +214,15 @@ class Realm(object):
 
         if inline_styled('bottom'):
             self.cursor.y += bottom
-            update_cursor('x')
+            # update_cursor('x')
 
         if inline_styled('left'):
             x += left
 
-        if inline_styled('trightop'):
+        if inline_styled('right'):
             text = text + " " * right
 
-        #| AND FINALLY, Write the text :>
+        # AND FINALLY, Write the text :>
         self.realm.addstr(y, x, text)
         self.refresh()
 
@@ -258,12 +258,12 @@ class Realm(object):
         """
 
         # TODO ------------------------------------------------------- > w <
-        #  * Add arrow keys functionality
+        # Add arrow keys functionality
 
-        #! This list will track all the characters that user pressed between ascii values 32 to 126
+        # This list will track all the characters that user pressed between ascii values 32 to 126
         string = []
 
-        #! Temporarily Disable the text echoing, specifically for curses.window.getch() 
+        # Temporarily Disable the text echoing, specifically for curses.window.getch() 
         curses.noecho()
 
         if len(coordinates) not in (0, 2):
@@ -282,7 +282,7 @@ class Realm(object):
         cursor_y = y + self.has_border
         cursor_x = x + self.has_border
 
-        #! Determine the maximum amount of inputtable characters
+        # Determine the maximum amount of inputtable characters
         border_length = (self.has_border + self.has_border)
         max_ch = (self.maxy - border_length - self.cursor.x) * (self.maxx - border_length - self.cursor.y)
 
@@ -293,60 +293,60 @@ class Realm(object):
         else:
             length = max_ch
 
-        #! Enter a loop and stay inside until Enter Key(ascii value 10) is pressed 
+        # Enter a loop and stay inside until Enter Key(ascii value 10) is pressed 
         while True:
 
-            #! Move the cursor 
+            # Move the cursor 
             self.realm.move(cursor_y, cursor_x)
 
-            #! This guy is the one who has been listening almost all the keyboard events 
+            # This guy is the one who has been listening almost all the keyboard events 
             ch = self.realm.getch()
 
-            #! If the received character is a backspace 
+            # If the received character is a backspace 
             if ch == 127 and len(string) != 0:
 
-                #! Remove the latest character from the list
+                # Remove the latest character from the list
                 string.pop()
 
-                #! Update the cursors
+                # Update the cursors
                 if cursor_x == self.has_border:
                     cursor_y -= 1
                     cursor_x = self.maxx - self.has_border
                 cursor_x -= 1
 
-                #! Update the screen
+                # Update the screen
                 self.realm.addch(cursor_y, cursor_x, " ")
 
 
-            #! If the received character is a typable letter(ascii value 32 to 126)
+            # If the received character is a typable letter(ascii value 32 to 126)
             elif 32 <= ch <= 126 and len(string) < length:
 
-                #! Append the received character to the list
+                # Append the received character to the list
                 string.append(chr(ch))
 
-                #! Update the screen
+                # Update the screen
                 self.realm.addch(cursor_y, cursor_x, chr(ch))
 
-                #! Update the cursors
+                # Update the cursors
                 if cursor_x == self.maxx - 1 - self.has_border:
                     cursor_x = self.has_border
                     cursor_y += 1
 
                 else: cursor_x += 1
 
-            #! If the received character is an Enter
+            # If the received character is an Enter
             elif ch == 10:
 
-                #! Leave the loop
+                # Leave the loop
                 break
 
-        #! Re-enable the enchoing for other curses.window.get* methods
+        # Re-enable the enchoing for other curses.window.get* methods
         curses.echo()
 
-        #! Update self.cursors (not better_getstr's cursors)
+        # Update self.cursors (not better_getstr's cursors)
         self.cursor.y = cursor_y - 1
 
-        #! Finally return the list of received characters 
+        # Finally return the list of received characters 
         return "".join(string)
     
     def barrier(
@@ -361,8 +361,8 @@ class Realm(object):
         """
 
         # TODO ------------------------------------------------------ > w <
-        #  * Make a own border function, the Curses one does not support all 
-        #  * characters. (Raises OverflowError)
+        # Make a own border function, the Curses one does not support all 
+        # characters. (Raises OverflowError)
 
         self.border.all          = kwargs.get('all', self.border.all)
         self.border.left         = kwargs.get('left', self.border.left )
@@ -400,7 +400,7 @@ class Realm(object):
         elif not activate: erase()
 
 
-#$ -----------------------------------------------------------------
+# -----------------------------------------------------------------
 
 
     def __Default_Configurations(self):
