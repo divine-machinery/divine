c7d42a2965484b59381a96900a5c06883789eed5
biscuit <divine.machinery@proton.me>
2025-06-15 21:06:29 +0630

    [+] add lazy getter and setters for layouts
    layouts doesn't only refer to the Layout class but also
    the layouts of all Domains that use Layout class.

--- divine/domains/heaven.py
+++ divine/domains/heaven.py
@@ -1,6 +1,7 @@
 from ..utilities import types as Type
 from . import STDSCR
 from ..layout import Layout
+from typing import Tuple
 
 
 class Heaven(object):
@@ -13,7 +14,61 @@ class Heaven(object):
 
     ):
 
+        self.parent = parent
+
         self.layout = Layout(self,
             coordinate = (y, x),
             width = width, height = height
         )
+
+
+    # ===== lazy getters and setters or whatever it is =====
+    
+    # -- coordinates
+
+    @property
+    def y(self) -> int:
+        return self.layout.y
+
+    @y.setter
+    def y(self, value: Type.LValue) -> None:
+        self.layout.y = value
+
+
+    @property
+    def x(self) -> int:
+        return self.layout.x
+
+    @x.setter
+    def x(self, value: Type.LValue) -> None:
+        self.layout.x = value
+    
+    # -- height
+
+    @property
+    def height(self) -> int:
+        return self.layout.height
+
+    @height.setter
+    def height(self, value: Type.LValue) -> None:
+        self.layout.height = value
+    
+    # -- width
+
+    @property
+    def width(self) -> int:
+        return self.layout.width
+
+    @width.setter
+    def width(self, value: Type.LValue) -> None:
+        self.layout.width = value
+
+    # -- extras
+
+    @property
+    def coordinate(self) -> Tuple[int, int]:
+        return self.layout.coordinate
+
+    @coordinate.setter
+    def coordinate(self, coordinate: Type.Coordinate) -> None:
+        self.layout.coordinate = coordinate
--- divine/layout.py
+++ divine/layout.py
@@ -1,4 +1,5 @@
 from .utilities import types as Type
+from typing import Tuple
 
 
 # the intention of this object is to maintain the layouts of 
@@ -18,3 +19,55 @@ class Layout(object):
         self.__y, self.__x = coordinate
         self.__height = height
         self.__width = width
+
+
+    # ===== lazy getters and setters or whatever it is =====
+    
+    # -- coordinates
+
+    @property
+    def y(self) -> int:
+        return self.__y if self.__y is not None else self.source.parent.y
+
+    @y.setter
+    def y(self, value: Type.LValue) -> None:
+        self.__y = value
+
+
+    @property
+    def x(self) -> int:
+        return self.__x if self.__x is not None else self.source.parent.x
+
+    @x.setter
+    def x(self, value: Type.LValue) -> None:
+        self.__x = value
+    
+    # -- height
+
+    @property
+    def height(self) -> int:
+        return self.__height if self.__height is not None else self.source.parent.height
+
+    @height.setter
+    def height(self, value: Type.LValue) -> None:
+        self.__height = value
+
+    # -- width
+
+    @property
+    def width(self) -> int:
+        return self.__width if self.__width is not None else self.source.parent.width
+
+    @width.setter
+    def width(self, value: Type.LValue) -> None:
+        self.__width = value
+
+    # -- extras
+
+    @property
+    def coordinate(self) -> Tuple[int, int]:
+        return (self.y, self.x)
+
+    @coordinate.setter
+    def coordinate(self, coordinate: Type.Coordinate) -> None:
+        self.y, self.x = coordinate
